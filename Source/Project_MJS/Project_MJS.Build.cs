371a4c7c6ff2039f35776cd7b19f98a69c71c540
YongseokPark1120 <ssters999@outlook.kr>
2026-06-13 23:29:28 +0900

    [#10] FMod 플러그인 추가(Niagara Fmod 포함) - WIndows 모듈

--- Source/Project_MJS/Project_MJS.Build.cs
+++ Source/Project_MJS/Project_MJS.Build.cs
@@ -8,7 +8,15 @@ public class Project_MJS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"FMODStudio"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
